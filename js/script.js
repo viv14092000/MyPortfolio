831c64088153fc1f2995ab26d3e85bd907e47409
Vivek Burnwal <98404783+viv14092000@users.noreply.github.com>
2024-02-04 12:50:58 +0530

    Portfolio added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//  typing animation
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Web Designer","Problem Solver","Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
